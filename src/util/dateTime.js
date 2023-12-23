77732ce6dad4bac4209e74d39c00976a6af283ba
AKINICEO <hieu46714@gmail.com>
2023-12-23 10:49:04 +0700

    up giao dien

--- src/util/dateTime.js
+++ src/util/dateTime.js
@@ -7,7 +7,7 @@ const handleDatetime = (datetime) => {
     };
     const datenow = new Date(datetime);
     const date = datenow.toLocaleDateString("en-GB");
-    const time = datenow.getHours() +":"+ datenow.getMinutes();
+    const time = datenow.getHours() +" : "+ datenow.getMinutes();
     return date + " - " + time;
 };
 export default handleDatetime;
