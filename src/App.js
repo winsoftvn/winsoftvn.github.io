1a71092b413edca27d385044672035458e966708
Winsoft Việt Nam <150001678+winsoftvn@users.noreply.github.com>
2024-01-10 15:18:53 +0700

    app js

--- src/App.js
+++ src/App.js
@@ -4,49 +4,53 @@ import LayoutPageLogin from "./layouts/LayoutPageLogin";
 import LayoutPageMain from "./layouts/LayoutPageMain";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-
+import LoginPage from "./pages/Login";
 function App() {
     const location = useLocation();
-
-    return (
-        <>
-            <Routes
-            // location={location} key={location.pathname}
-            >
-                {PageWeb.map((route, index) => {
-                    const Page = route.page;
-                    let Layout = LayoutPageLogin;
-                    if (route.isMainLayout) {
-                        Layout = LayoutPageMain;
-                    }
-                    return (
-                        <Route
-                            key={index}
-                            path={route.path}
-                            element={
-                                <Layout>
-                                    <Page />
-                                </Layout>
-                            }
-                        />
-                    );
-                })}
-            </Routes>
-            <ToastContainer
-                position="top-right"
-                autoClose={1500}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-            />
-            <ToastContainer />
-        </>
-    );
+    const token = localStorage.getItem("token");
+    if (token == null) {
+        return (<LoginPage />);
+    } else {
+        return (
+            <>
+                <Routes
+                // location={location} key={location.pathname}
+                >
+                    {PageWeb.map((route, index) => {
+                        const Page = route.page;
+                        let Layout = LayoutPageLogin;
+                        if (route.isMainLayout) {
+                            Layout = LayoutPageMain;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+                <ToastContainer
+                    position="top-right"
+                    autoClose={1500}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                />
+                <ToastContainer />
+            </>
+        );
+    }
 }
 
 export default App;
