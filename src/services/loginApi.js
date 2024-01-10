7a58351f661a15cc5f954127e510ced87ca039b5
Winsoft Việt Nam <150001678+winsoftvn@users.noreply.github.com>
2024-01-10 15:22:11 +0700

    api login

---
+++ src/services/loginApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import REACT_LOCALHOST from "./host";
+import { authHeader } from '../components/helpers';
+const loginAPI = {
+    
+    login: (user) => {
+        return axios.post(`${REACT_LOCALHOST}/api/login`, user);
+    },
+    profileFetch: () => {
+        let token = JSON.parse(localStorage.getItem('token'));
+        return axios.post(`${REACT_LOCALHOST}/api/profile-fetch`, { "accessToken": token} );
+    },
+   
+};
+
+export default loginAPI;
